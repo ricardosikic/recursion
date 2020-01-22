851cbb868d5b8a1ce999e4df70ea0e2b1ed611e6
ricardosikic <ricardo.sikic@gmail.com>
2020-01-22 14:20:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+// la recursion no es mas que una funcion que se llama a si misma hasta alcanzar su objetivo, podriamos hacer un ejemplo de factorial > recordemos que el factorial es multiplicar desde el numero hasta llegar a 1.
+
