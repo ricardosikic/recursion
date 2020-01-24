692cd16c5f78eb27f5779215d30d0ec889a63e4a
ricardosikic <ricardo.sikic@gmail.com>
2020-01-24 12:59:50 +0000

    first

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // la recursion no es mas que una funcion que se llama a si misma hasta alcanzar su objetivo, podriamos hacer un ejemplo de factorial > recordemos que el factorial es multiplicar desde el numero hasta llegar a 1.
+function suma(n) {
+  if (n <= 1) return 1;
+  return n * suma(n-1);
+}
+
+suma(5);
 
